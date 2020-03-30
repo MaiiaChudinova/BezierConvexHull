1341fc09badc66117a8c49310f16b1dce7eb6632
MaiiaChudinova <maya.chudinova@unicyb.kiev.ua>
2020-03-31 00:18:43 +0300

    Improved view styling, got rid of unnecessary user control

--- BezierConvexHull/BezierConvexHull/ViewModel/MainViewModel.cs
+++ BezierConvexHull/BezierConvexHull/ViewModel/MainViewModel.cs
@@ -8,5 +8,25 @@ namespace BezierConvexHull
 {
     public class MainViewModel : BaseViewModel
     {
+        public List<PointSetViewModel> SamplePointSets;
+
+        public RelayCommand NextSampleCommand;
+
+        public RelayCommand GenerateRandomSampleCommand;
+
+        public RelayCommand ShowHelpCommand;
+
+        public int MaxPointsNumber { get; }
+
+        // TODO: Delete these properties and use element binding in XAML only instead of data context binding
+        public int MaxPointsXLowerBound { get; }
+        public int MinPointsXLowerBound { get; }
+        public int MaxPointsXUpperBound { get; }
+        public int MinPointsXUpperBound { get; }
+        public int MaxPointsYLowerBound { get; }
+        public int MinPointsYLowerBound { get; }
+        public int MaxPointsYUpperBound { get; } = 400;
+        public int MinPointsYUpperBound { get; } = 10;
+       
     }
 }
---
+++ BezierConvexHull/BezierConvexHull/ViewModel/PointSetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezierConvexHull
+{
+    public class PointSetViewModel : BaseViewModel
+    {
+        public int PointsNumber { get; set; }
+        public int MinX { get; set; }
+        public int MaxX { get; set; }
+        public int MinY { get; set; }
+        public int MaxY { get; set; }   
+    }
+}
