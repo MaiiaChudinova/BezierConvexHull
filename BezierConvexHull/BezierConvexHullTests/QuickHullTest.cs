e611d0e2f52884fba22f9b6cf7c1c53f4d9fcb59
MaiiaChudinova <maya.chudinova@unicyb.kiev.ua>
2020-04-01 00:56:09 +0300

    Added MSTest project

---
+++ BezierConvexHull/BezierConvexHullTests/QuickHullTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BezierConvexHullTests
+{
+    [TestClass]
+    public class QuickHullTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
